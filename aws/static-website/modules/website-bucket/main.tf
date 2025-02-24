0387868923cf0a90aa04d71300dd702bcfa937af
msutton150 <155597218+msutton150@users.noreply.github.com>
2025-02-24 08:06:41 -0500

    Add files via upload

---
+++ aws/static-website/modules/website-bucket/main.tf
@@ -0,0 +1,41 @@
+resource "random_string" "random" {
+  length = 16
+  special = false
+  upper = false
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "${var.bucket_name}-${random_string.random.result}"
+
+  force_destroy = true
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.bucket_name}-${random_string.random.result}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = var.home_page
+    error_document = var.error_page
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "public-read"
+}
---
+++ kubernetes/gke/terraform.tfvars
@@ -0,0 +1,4 @@
+# GCP Settings
+project = "space-invaders"
+project_id = "your-terraform-project-id"
+gcp_zone   = "us-central1-a"
