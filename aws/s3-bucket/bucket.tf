655a7f66952646f2c166928702e905a9d5a8c05a
msutton150 <155597218+msutton150@users.noreply.github.com>
2025-03-04 10:38:38 -0500

    Update bucket.tf

--- aws/s3-bucket/bucket.tf
+++ aws/s3-bucket/bucket.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
