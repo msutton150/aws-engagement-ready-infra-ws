4a2d6ace704ede901b4c0f3f5a7bf07e0430745e
msutton150 <155597218+msutton150@users.noreply.github.com>
2025-05-01 10:30:07 -0400

    Update cluster.tf

--- kubernetes/eks/cluster.tf
+++ kubernetes/eks/cluster.tf
@@ -5,11 +5,16 @@ module "eks" {
   version = "18.26.6"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.22"
+  cluster_version = "1.32"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
+  
+  # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1810
+  node_security_group_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = null
+  }
+  
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
 
