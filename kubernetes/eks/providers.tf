dfa14cb3f15946874355027eb16eb0592a8c10c7
msutton150 <155597218+msutton150@users.noreply.github.com>
2025-05-01 10:31:05 -0400

    Update providers.tf

--- kubernetes/eks/providers.tf
+++ kubernetes/eks/providers.tf
@@ -1,49 +1,31 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.36.1"
+    aws        = {
+      source   = "hashicorp/aws"
+      version  = "4.36.1"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+    random     = {
+      source   = "hashicorp/random"
+      version  = "3.1.0"
     }
     kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      source   = "hashicorp/kubernetes"
+      version  = "~> 2.0"
     }
   }
-
 }
 
 provider "aws" {
-  region  = var.region
-}
-
-data "terraform_remote_state" "eks" {
-  backend = "local"
-
-  config = {
-    path = "./terraform.tfstate"
-  }
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
+  region = var.region
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  host                   = try(module.eks.cluster_endpoint, "foo")
+  cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "bar")
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      data.aws_eks_cluster.cluster.name
-    ]
+    api_version          = "client.authentication.k8s.io/v1"
+    args                 = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    command              = "aws"
   }
 }
